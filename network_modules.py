ac4e67bbcb3d54b19871caeaf10e1c719b10c416
Rohit Konda <platypus96@gmail.com>
2017-07-04 16:57:13 -0400

    Started Repo for Graph Estimates

--- network_modules.py
+++ network_modules.py
@@ -47,18 +47,18 @@ class nmodel:
         c = 0 # counter if f depends on the edge
         for i in range(0,len(state)):
             sumEdge = np.array(dev[i].tolist()[0])
-            if self.G[i+1]:
-                for j in self.G[i+1]:
+            if self.G[i]:
+                for j in self.G[i]:
                     if isinstance(self.f, col.Iterable):
                         if len(self.f) == len(self.x):
-                            sumEdge += self.f[i](state[i],state[j-1])
+                            sumEdge += self.f[i](state[i],state[j])
                         elif len(self.f) == self.G.number_of_edges():
-                            sumEdge += self.f[c](state[i],state[j-1])
+                            sumEdge += self.f[c](state[i],state[j])
                             c += 1
                         else:
                             raise ValueError('length of f must either be equal to the number of nodes or edges')
                     elif callable(self.f):
-                        sumEdge += self.f(state[i],state[j-1])
+                        sumEdge += self.f(state[i],state[j])
                     else:
                         raise ValueError('f must be either be iterable or callable')
             if isinstance(self.h, col.Iterable):
@@ -157,10 +157,10 @@ def create_network(ntype = '', n = 1, k = 1, p = 1, m = 1):
     elif ntype == 'BA':
         return nx.barabasi_albert_graph(n, m)
     elif ntype == 'SCC':
-        nodes = [1,2,3,4,5,6]
+        nodes = [0,1,2,3,4,5]
         G = nx.Graph()
         G.add_nodes_from(nodes)
-        G.add_edges_from([(1,2),(2,3),(1,6),(1,3),(3,4),(4,5),(4,6),(5,6)])
+        G.add_edges_from([(0,1),(1,2),(0,5),(0,2),(2,3),(3,4),(3,5),(4,5)])
         return G
     else:
         raise ValueError('not a valid network')
@@ -225,67 +225,5 @@ def PSD(signal, fs=1.0, window='hann', nperseg=None, noverlap=None, nfft=None):
     plt.ylabel('PSD [V**2/Hz]')
     plt.show()
 
-#runs functions across all signals and outputs matrix of results
-def cross_func(states, func):
-    M = np.zeros((len(states), len(states)))
-    for i in range(len(states)):
-        for j in range(len(states)):
-            M[i,j] = func(states[i,:],states[j,:])
-    return np.matrix(M)
-
-#Source: https://github.com/scikit-learn/scikit-learn/blob/14031f6/sklearn/metrics/cluster/supervised.py#L531
-#Mutual Information
-def mutual_info_score(labels_true, labels_pred, contingency=None):
-    if contingency is None:
-        labels_true, labels_pred = check_clusterings(labels_true, labels_pred)
-        contingency = contingency_matrix(labels_true, labels_pred, sparse=True)
-    else:
-        contingency = check_array(contingency,
-                                  accept_sparse=['csr', 'csc', 'coo'],
-                                  dtype=[int, np.int32, np.int64])
-
-    if isinstance(contingency, np.ndarray):
-        # For an array
-        nzx, nzy = np.nonzero(contingency)
-        nz_val = contingency[nzx, nzy]
-    elif sp.issparse(contingency):
-        # For a sparse matrix
-        nzx, nzy, nz_val = sp.find(contingency)
-    else:
-        raise ValueError("Unsupported type for 'contingency': %s" %
-                         type(contingency))
-
-    contingency_sum = contingency.sum()
-    pi = np.ravel(contingency.sum(axis=1))
-    pj = np.ravel(contingency.sum(axis=0))
-    log_contingency_nm = np.log(nz_val)
-    contingency_nm = nz_val / contingency_sum
-    # Don't need to calculate the full outer product, just for non-zeroes
-    outer = pi.take(nzx) * pj.take(nzy)
-    log_outer = -np.log(outer) + log(pi.sum()) + log(pj.sum())
-    mi = (contingency_nm * (log_contingency_nm - log(contingency_sum)) +
-          contingency_nm * log_outer)
-    return mi.sum()
-
-#PLV
-def phase_locking_value(x,y):
-    complex_phase_diff = np.exp(np.complex(0,1)*(x - y))
-    plv = np.abs(np.sum(complex_phase_diff))/len(x)
-    return plv
-
-#coherence
-coh = lambda x,y: np.mean(sig.coherence(x,y)[1])
-
-#correlation
-cor = np.correlate
-
 #get time courses from specified part of the vector state
 reduce_state = lambda i, x:  np.matrix([row[i] for row in x])
-
-#Nonlinear measures
-#import nolds.sampen as as sp
-#import nolds.corr_dim as cd
-#import nolds.lyap_r as lp
-
-#Granger Causality
-#import nitime.analysis.granger as GC
